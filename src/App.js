140e4044e58235013b017565be42c6c46890f1ac
LuongLuong <luong2443@gmail.com>
2020-06-25 17:41:05 -0500

    Create Logo and others

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Homepage from './page/homepage/homepage.compo';
 import Items from './page/shop/shop.compo.jsx';
+import Header from './components/header/header-functions.compo.jsx';
 
 import { BrowserRouter,Switch, Route } from 'react-router-dom';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div >
    <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage}></Route>/>
         <Route  path='/shop' component={Items}></Route>/>
---
+++ src/components/header/header-functions.compo.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {ReactComponent as Logo} from '../../Logo/star-wars.svg';
+import {Link} from "react-router-dom";
+import './header-function.style.scss';
+
+const Header= () =>(
+    <div className='header'>
+
+        <Link className='logo-container' to='/'>
+            <Logo className='logo'>
+            </Logo>
+        </Link>
+
+            <div className='menu-top-options'>
+                <Link className='option' to='/shop'>
+                    Shop
+                </Link>
+                <Link className='option' to='/shop'>
+                    Contact Us
+                </Link>
+                <Link className='option' to='/shop'>
+                    Account
+                </Link>
+            </div>
+    </div>
+)
+export default Header;
