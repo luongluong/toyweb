4ec74d7139ea190282ce5382e8db004bee4a84ef
Luong Luong <luong2443@gmail.com>
2021-09-16 19:06:44 -0500

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAGtrXbRbW3G4OuwG3wmW3lFzKQw3Ylu7M",
-    authDomain: "rebelsw-ee848.firebaseapp.com",
-    databaseURL: "https://rebelsw-ee848.firebaseio.com",
-    projectId: "rebelsw-ee848",
-    storageBucket: "rebelsw-ee848.appspot.com",
-    messagingSenderId: "928180662438",
-    appId: "1:928180662438:web:b7857357f22629a7810467",
-    measurementId: "G-ETKEY24BVN"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   // function to store users` info in database
